54767aa1a5221b46fd96868dcdd8f6b2d051e535
Andrew Chan <z5589701@ad.unsw.edu.au>
2025-05-27 16:21:27 +1000

    Changed scrolling from window to wheel type

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const slides = document.querySelectorAll(".slide");
 const aboutText = document.getElementById("aboutText");
 
-let lastSlide = -1;
+let currentSlide = 0;
+let isScrolling = false;
 
-window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  const screenHeight = window.innerHeight;
-  const currentSlide = Math.floor(scrollY / screenHeight);
-
-  if (currentSlide !== lastSlide && slides[currentSlide]) {
-    lastSlide = currentSlide;
-
+function updateSlide(index) {
+  if (slides[index]) {
     aboutText.style.opacity = 0;
     setTimeout(() => {
-      aboutText.innerHTML = slides[currentSlide].dataset.text;
+      aboutText.innerHTML = slides[index].dataset.text;
       aboutText.style.opacity = 1;
     }, 200);
   }
+}
+
+document.querySelector(".circle").addEventListener("wheel", (e) => {
+  e.preventDefault();
+
+  if (isScrolling) return;
+  isScrolling = true;
+
+  if (e.deltaY > 0 && currentSlide < slides.length - 1) {
+    currentSlide++;
+  } else if (e.deltaY < 0 && currentSlide > 0) {
+    currentSlide--;
+  }
+
+  updateSlide(currentSlide);
+
+  setTimeout(() => {
+    isScrolling = false;
+  }, 500);
 });
